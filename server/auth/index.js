26ba32d63643a3c01d04c0aedf130b7b4ae7315b
Mark Glasman <glasman.mark@gmail.com>
2023-12-12 16:29:04 -0500

    worked on login and register routes

---
+++ server/auth/index.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+
+const prisma = new PrismaClient();
+const saltRounds = 10;
+
+router.post("/register", async (req, res) => {
+  const unhashed = req.body.password;
+
+  console.log("Unhashed: ", unhashed);
+
+  const hashed = await bcrypt.hash(unhashed, saltRounds);
+
+  console.log("Hashed: ", hashed);
+
+  try {
+    console.log(req.body);
+    const result = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: hashed,
+      },
+    });
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+//works for newly registered users but not ones made by the seed, will have to go back
+//and rewrite how we store seeded passwords
+router.post("/login", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  if (user) {
+    const myPlaintextPassword = req.body.password;
+    const isMatch = await bcrypt.compare(myPlaintextPassword, user.password);
+    console.log(myPlaintextPassword)
+    console.log(user.password)
+    if (isMatch) {
+      res.send("Successfully logged in!");
+    } else {
+      res.sendStatus(401);
+    }
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
@@ -9,6 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.use("/api", require("./api"));
+app.use("/auth", require("./auth"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
